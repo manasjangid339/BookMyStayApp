f3ce3ed0c2f4bc44d108bdb2bc6d6a039eb1b6bc
manasjangid339 <mj3396@srmist.edu.in>
2026-03-13 15:55:48 +0530

    UC1

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,16 @@
 public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Hotel Booking System v1.0     ");
+        System.out.println("=================================");
+
+        // Inform user that application has started successfully
+        System.out.println("Application started successfully.");
+
+        // Inform that the program is terminating
+        System.out.println("Thank you for using Book My Stay!");
+    }
 }
